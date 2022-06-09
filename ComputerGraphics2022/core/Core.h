329f0981388dffc43c76d5684c9dbdb548b0ac5d
doomedboi <akulenko.vv@students.dvfu.ru>
2022-06-09 13:44:38 +1000

    Arch draft
    Core class draft w/ a little bit input manager impl

---
+++ ComputerGraphics2022/core/Core.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "../render/common.h"
+#include "../render/Render.h"
+#include "../render/Core_Camera.h"
+#include "InputManager.h"
+#include <memory>
+
+class Core {
+public:
+    static Core& Singleton() { static Core instance; return instance; }
+    void SceneUpdate();
+    void ScenePostUpdate(); // draw is here
+    /* members getters */
+    CCamera& GetViewCamera() { return camera_; }
+    void SetViewCamera(CCamera& camera) { camera_ = camera; }
+    void InitRender(int w, int h, int bitsPerPix, const std::string& title) {
+        render_ = std::make_unique<Renderer>(w, h, bitsPerPix, title);
+    }
+    void InitCamera(const math::vec3& pos, const math::vec3& front = math::vec3(0,0,0),
+        const math::vec3& up = math::vec3(0, 1, 0), const float fov = 45.f, 
+        const float yaw = default_yaw, const float pitch = default_pitch) {
+        camera_ = CCamera(pos, front, up, fov, yaw, pitch);
+        camera_.SetRenderProjectMatrix(fov,
+            render_->getWnd()->getSize().x / render_->getWnd()->getSize().y, 0.1f, 100.f);
+    }
+    Core() : render_(nullptr), camera_(math::vec3(0, 0, 3.f)) {}
+    Renderer& GetRender() { return *render_; }
+    Core(const Core&&) = delete;
+    Core(Core&&) = delete;
+    void operator=(const Core&) = delete;
+private:
+    std::unique_ptr<Renderer> render_; // draw stuff getted from scene manager
+    CCamera camera_;
+    InputManager inputManager_;
+};
---
+++ ComputerGraphics2022/core/InputManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+/* Note for me: it's class that just listens and updates it's state.
+ In case of realisation I must just make class that trigerring on the input.
+ Do sfml's functuunality already implement this? After that I hafta add posibility 
+ to listen changes. As I assume */
+class InputManager final {
+public:
+    bool IsKeyPressed(sf::Keyboard::Key key) const {
+        return sf::Keyboard::isKeyPressed(key);
+    }
+
+    bool IsKeyReleased(sf::Keyboard::Key p_key) const;
+    bool IsMouseButtonPressed(sf::Mouse::Button p_button) const;
+    bool IsMouseButtonReleased(sf::Mouse::Button p_button) const;
+    std::pair<int, int> GetMousePosition() const {
+        auto [x, y] = sf::Mouse::getPosition();
+        return { x, y };
+    }
+    void ClearEvents();
+};
